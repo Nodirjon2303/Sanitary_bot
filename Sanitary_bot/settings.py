af4b4aa7a3b7297f8b7351001e6e7c857fcbe6e9
Nodirjon Ro'zimurodov <82306944+Nodirjon2303@users.noreply.github.com>
2021-11-05 22:05:10 +0500

    nodir

--- Sanitary_bot/settings.py
+++ Sanitary_bot/settings.py
@@ -71,25 +71,25 @@ WSGI_APPLICATION = 'Sanitary_bot.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'myproject',
+#         'USER': 'algoritm',
+#         'PASSWORD': 'Nodir2303',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
+
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myproject',
-        'USER': 'algoritm',
-        'PASSWORD': 'Nodir2303',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
